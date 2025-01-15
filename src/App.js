38559df0f992715bec5835510cf63bb1842f3e54
hjkim0905 <eax9952@gmail.com>
2025-01-15 18:43:15 +0900

    [Fix] : 다시돌아옴

--- src/App.js
+++ src/App.js
@@ -1,78 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ParallaxProvider } from 'react-scroll-parallax';
+import Onboarding from './Onboarding/Onboarding';
 import MainPage from './pages/MainPage';
-import SignUpPage from './components/SignUpPage';
 import AuthCallback from './components/AuthCallback';
-import AddSermonPage from './components/AddSermonPage';
-import QuickReadingPage from './components/QuickReadingPage';
-import SermonListPage from './components/SermonListPage';
-import SearchPage from './components/SearchPage';
-import BookmarkPage from './components/BookmarkPage';
-
-const PrivateRoute = ({ children }) => {
-    const isAuthenticated = localStorage.getItem('UID') !== null;
-    return isAuthenticated ? children : <Navigate to="/signup" />;
-};
+import SignUpPage from './components/SignUpPage';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/signup" element={<SignUpPage />} />
-                <Route path="/auth/callback" element={<AuthCallback />} />
-                <Route path="/" element={<Navigate to="/signup" />} />
-
-                {/* Protected Routes */}
-                <Route
-                    path="/main"
-                    element={
-                        <PrivateRoute>
-                            <MainPage />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/add-sermon"
-                    element={
-                        <PrivateRoute>
-                            <AddSermonPage />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/quick-reading"
-                    element={
-                        <PrivateRoute>
-                            <QuickReadingPage />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/sermon-list"
-                    element={
-                        <PrivateRoute>
-                            <SermonListPage />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/search"
-                    element={
-                        <PrivateRoute>
-                            <SearchPage />
-                        </PrivateRoute>
-                    }
-                />
-                <Route
-                    path="/bookmark"
-                    element={
-                        <PrivateRoute>
-                            <BookmarkPage />
-                        </PrivateRoute>
-                    }
-                />
-            </Routes>
-        </Router>
+        <ParallaxProvider>
+            <Router basename="/eax9952">
+                <Routes>
+                    <Route path="/" element={<Onboarding />} />
+                    <Route path="/auth" element={<AuthCallback />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/main/*" element={<MainPage />} />
+                </Routes>
+            </Router>
+        </ParallaxProvider>
     );
 }
 
