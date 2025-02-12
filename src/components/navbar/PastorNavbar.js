0cccaa8af51a3b9388b4941432c6eca5e9f07e3b
naimkim <naimk5451@gmail.com>
2025-02-12 15:50:34 +0900

    STYLE logo expandable

--- src/components/navbar/PastorNavbar.js
+++ src/components/navbar/PastorNavbar.js
@@ -1,73 +1,109 @@
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { Zap, Search, LogOut, PlusCircle, BookOpen, User, ChevronLeft } from 'lucide-react';
-import { useRecoilState } from 'recoil';
-import { isNavExpandedState } from '../../recoil/atoms';
-import LogoWhite from '../../assets/LogoWhite.png';
-import { NavContainer, LogoContainer, Logo, NavItems, NavItem, LogoutButton, ToggleButton } from './NavbarStyles';
-import { useLogout } from './navbarUtils';
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  Zap,
+  Search,
+  LogOut,
+  PlusCircle,
+  BookOpen,
+  User,
+  ChevronLeft,
+} from "lucide-react";
+import { useRecoilState } from "recoil";
+import { isNavExpandedState } from "../../recoil/atoms";
+import LogoExpanded from "../../assets/LogoLong.svg";
+import LogoUnexpanded from "../../assets/Logo.svg";
+import {
+  NavContainer,
+  LogoContainer,
+  Logo,
+  NavItems,
+  NavItem,
+  LogoutButton,
+  ToggleButton,
+} from "./NavbarStyles";
+import { useLogout } from "./navbarUtils";
 
 const PastorNavbar = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const logout = useLogout();
-    const [isExpanded, setIsExpanded] = useRecoilState(isNavExpandedState);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const logout = useLogout();
+  const [isExpanded, setIsExpanded] = useRecoilState(isNavExpandedState);
 
-    const isActive = (path) => {
-        if (path === '/sermon-list') {
-            return location.pathname.includes('/main/sermon-list');
-        }
-        return location.pathname === `/main${path}` || (path === '/quick-reading' && location.pathname === '/main');
-    };
-
+  const isActive = (path) => {
+    if (path === "/sermon-list") {
+      return location.pathname.includes("/main/sermon-list");
+    }
     return (
-        <NavContainer isExpanded={isExpanded}>
-            <ToggleButton isExpanded={isExpanded} onClick={() => setIsExpanded(!isExpanded)}>
-                <ChevronLeft />
-            </ToggleButton>
-            <LogoContainer>
-                <Logo src={LogoWhite} alt="Bibly Logo" onClick={() => navigate('/main')} isExpanded={isExpanded} />
-            </LogoContainer>
-            <NavItems>
-                <NavItem
-                    onClick={() => navigate('/main/quick-reading')}
-                    active={isActive('/quick-reading')}
-                    isExpanded={isExpanded}
-                >
-                    <Zap size={24} />
-                    <span>빠른 성경 읽기</span>
-                </NavItem>
-                <NavItem onClick={() => navigate('/main/search')} active={isActive('/search')} isExpanded={isExpanded}>
-                    <Search size={24} />
-                    <span>검색</span>
-                </NavItem>
-                <NavItem
-                    onClick={() => navigate('/main/add-sermon')}
-                    active={isActive('/add-sermon')}
-                    isExpanded={isExpanded}
-                >
-                    <PlusCircle size={24} />
-                    <span>설교 작성</span>
-                </NavItem>
-                <NavItem
-                    onClick={() => navigate('/main/sermon-list')}
-                    active={isActive('/sermon-list')}
-                    isExpanded={isExpanded}
-                >
-                    <BookOpen size={24} />
-                    <span>설교 목록</span>
-                </NavItem>
-                <NavItem onClick={() => navigate('/main/mypage')} active={isActive('/mypage')} isExpanded={isExpanded}>
-                    <User size={24} />
-                    <span>마이페이지</span>
-                </NavItem>
-            </NavItems>
-            <LogoutButton onClick={logout} isExpanded={isExpanded}>
-                <LogOut size={24} />
-                <span>로그아웃</span>
-            </LogoutButton>
-        </NavContainer>
+      location.pathname === `/main${path}` ||
+      (path === "/quick-reading" && location.pathname === "/main")
     );
+  };
+
+  return (
+    <NavContainer isExpanded={isExpanded}>
+      <ToggleButton
+        isExpanded={isExpanded}
+        onClick={() => setIsExpanded(!isExpanded)}
+      >
+        <ChevronLeft />
+      </ToggleButton>
+      <LogoContainer>
+        <Logo
+          src={isExpanded ? LogoExpanded : LogoUnexpanded}
+          alt="Bibly Logo"
+          onClick={() => navigate("/main")}
+          isExpanded={isExpanded}
+        />
+      </LogoContainer>
+      <NavItems>
+        <NavItem
+          onClick={() => navigate("/main/quick-reading")}
+          active={isActive("/quick-reading")}
+          isExpanded={isExpanded}
+        >
+          <Zap size={24} />
+          <span>빠른 성경 읽기</span>
+        </NavItem>
+        <NavItem
+          onClick={() => navigate("/main/search")}
+          active={isActive("/search")}
+          isExpanded={isExpanded}
+        >
+          <Search size={24} />
+          <span>검색</span>
+        </NavItem>
+        <NavItem
+          onClick={() => navigate("/main/add-sermon")}
+          active={isActive("/add-sermon")}
+          isExpanded={isExpanded}
+        >
+          <PlusCircle size={24} />
+          <span>설교 작성</span>
+        </NavItem>
+        <NavItem
+          onClick={() => navigate("/main/sermon-list")}
+          active={isActive("/sermon-list")}
+          isExpanded={isExpanded}
+        >
+          <BookOpen size={24} />
+          <span>설교 목록</span>
+        </NavItem>
+        <NavItem
+          onClick={() => navigate("/main/mypage")}
+          active={isActive("/mypage")}
+          isExpanded={isExpanded}
+        >
+          <User size={24} />
+          <span>마이페이지</span>
+        </NavItem>
+      </NavItems>
+      <LogoutButton onClick={logout} isExpanded={isExpanded}>
+        <LogOut size={24} />
+        <span>로그아웃</span>
+      </LogoutButton>
+    </NavContainer>
+  );
 };
 
 export default PastorNavbar;
